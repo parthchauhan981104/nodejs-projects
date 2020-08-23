77cb287ee738f4e6a582a1f5e879f7d7d4ba88d8
Parth <gamerboy09pc@gmail.com>
2020-08-23 12:12:37 +0530

    mvc

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>e-Shopping application</h1>
+Multiple folders starting with 'app' (example - app1-vanillaNodejs), each has incremental versions of the application. 
---
+++ app4-mvc/models/product.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    //must use arrow function so 'this' doesnt lose context and refers to object based on the class
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) { // can call directly on the class without an object
+    getProductsFromFile(cb);
+  }
+};
+
